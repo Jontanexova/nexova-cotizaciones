c7d8844c0a78e017356a8c9a074d38d3f6bf916c
Jontanexova <jonathan.moreno@nexova.pe>
2026-04-19 14:38:20 -0500

    v2.20

--- src/hooks/useVendors.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback } from 'react';
-import { fetchVendors } from '../lib/db';
-import { useAsyncData } from './useAsyncData';
-
-export function useVendors() {
-  const fetcher = useCallback((signal: AbortSignal) => fetchVendors(signal), []);
-  const { data, loading, error, reload } = useAsyncData(fetcher, []);
-  return { vendors: data, loading, error, reload };
-}
