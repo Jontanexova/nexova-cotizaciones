847e76d6fc573d14a840256ac35af4f5a4e2b0f0
Jontanexova <jonathan.moreno@nexova.pe>
2026-04-23 23:01:20 -0500

    v2.27

--- src/hooks/useQuotes.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback } from 'react';
-import { fetchQuotes } from '../lib/db';
-import { useAsyncData } from './useAsyncData';
-
-export function useQuotes() {
-  const fetcher = useCallback((signal: AbortSignal) => fetchQuotes(signal), []);
-  const { data, loading, error, reload } = useAsyncData(fetcher, []);
-  return { quotes: data, loading, error, reload };
-}
